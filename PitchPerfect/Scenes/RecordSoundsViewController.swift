7a0f80c003b1efffb2bdc1eb388398ece5b76258
Baran Batuhan Karaoğuz <baran.karaoguz@ogr.sakarya.edu.tr>
2021-02-08 14:28:29 +0300

    Added new controllers and binding storyboard

---
+++ PitchPerfect/Scenes/RecordSoundsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by BARAN BATUHAN KARAOGUZ on 8.02.2021.
+//
+
+import UIKit
+
+class RecordSoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
